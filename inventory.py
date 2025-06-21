d8ef305a01ab7632fa44ffd50e729421fb36f433
LowryderPL <lowryderpl921@gmail.com>
2025-06-22 00:13:06 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,15 +1,24 @@
+class Item:
+    def __init__(self, name, description, rarity):
+        self.name = name
+        self.description = description
+        self.rarity = rarity
+
+    def display_info(self):
+        print(f"▶ {self.name} ({self.rarity})\n  {self.description}")
+
 class Inventory:
     def __init__(self):
         self.items = []
 
     def add_item(self, item):
         self.items.append(item)
-        print(f"Dodano do ekwipunku: {item}")
+        print(f"[+1] Zdobyto: {item.name} ({item.rarity})")
 
     def show_inventory(self):
-        print("=== TWÓJ EKWIPUNEK ===")
         if not self.items:
-            print("Ekwipunek jest pusty.")
+            print("⚠ Twój ekwipunek jest pusty.")
         else:
+            print("📦 Twój ekwipunek:")
             for item in self.items:
-                print(f"- {item}")
+                item.display_info()
