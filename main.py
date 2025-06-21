33f469f3443593270eb5139601fe82744f556aad
LowryderPL <lowryderpl921@gmail.com>
2025-06-21 23:52:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-dfrom game_engine import GameEngine
+from game_engine import GameEngine
 from menu import main_menu
+from map_system import MapSystem
 
 def main():
     print("=== FIROS: Magic & Magic ===")
     main_menu()
+    
     engine = GameEngine()
     engine.run()
 
+    map_system = MapSystem()
+    map_system.display_map()
+
 if __name__ == "__main__":
     main()
-    from map_system import MapSystem
-
-map_system = MapSystem()
-map_system.display_map()
