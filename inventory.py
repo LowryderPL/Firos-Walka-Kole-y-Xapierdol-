5bb049675ff8bd379c857ae3b57d7e2314b2ba2a
LowryderPL <lowryderpl921@gmail.com>
2025-06-21 23:46:52 +0200

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,1 +1,15 @@
-# Placeholder for inventory.py
+class Inventory:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+        print(f"Dodano do ekwipunku: {item}")
+
+    def show_inventory(self):
+        print("=== TWÓJ EKWIPUNEK ===")
+        if not self.items:
+            print("Ekwipunek jest pusty.")
+        else:
+            for item in self.items:
+                print(f"- {item}")
