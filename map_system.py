d84c1c0483d5e790f06df602293d4c240d51df50
LowryderPL <lowryderpl921@gmail.com>
2025-06-21 23:45:05 +0200

    Update map_system.py

--- map_system.py
+++ map_system.py
@@ -1,1 +1,8 @@
-# Placeholder for map_system.py
+def display_world_map():
+    print("=== MAPA ŚWIATA ===")
+    print("1. Kyloun")
+    print("2. Drekkul Fells")
+    print("3. Shadoween")
+    print("4. Rain")
+    print("5. Misweth Thalin")
+    print("6. Empire of Thalin")
