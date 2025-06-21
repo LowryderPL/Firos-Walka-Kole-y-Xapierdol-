0220132885dadf0b27ea3ecf5cbbc45c1d244485
LowryderPL <lowryderpl921@gmail.com>
2025-06-21 23:40:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-# Main script for running FIROS game
+from game_engine import GameEngine
+from menu import main_menu
 
-print('FIROS Game Loaded')
+def main():
+    print("=== FIROS: Magic & Magic ===")
+    main_menu()
+    engine = GameEngine()
+    engine.run()
+
+if __name__ == "__main__":
+    main()
