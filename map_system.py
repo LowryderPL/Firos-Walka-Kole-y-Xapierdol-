0ffddbbe60637a73310a7cd8453224d455ac15d0
LowryderPL <lowryderpl921@gmail.com>
2025-06-22 00:35:06 +0200

    Update map_system.py

--- map_system.py
+++ map_system.py
@@ -3,24 +3,15 @@ class MapSystem:
         self.regions = {
             "Północne Pogranicze": "The Eldrath",
             "Wielkie Pustkowia": "Valyria Confederacy",
-            "Thalijskie Wzgórza": "Empire of Thalin",
+            "Thaliijskie Wzgórza": "Empire of Thalin",
             "Zamarznięte Krainy": "K'Yoloun",
-            "Mroczne Góry": "Drëbkull Fells",
+            "Góry Orków": "Drekbull Kells",
             "Ziemie Cienia": "Shadoween",
             "Kraina Deszczu": "Rain",
-            "Las Mistrzów": "Miswehh Thalin"
+            "Lasy Mgieł": "Miswevh Thalin"
         }
         self.discovered = set()
 
     def display_map(self):
         print("\n=== MAPA ŚWIATA ===")
-        for region, faction in self.regions.items():
-            status = "✔ odkryto" if region in self.discovered else "❌ nieodkryte"
-            print(f"{region} ({faction}) — {status}")
-
-    def explore_region(self, region_name):
-        if region_name in self.regions:
-            self.discovered.add(region_name)
-            print(f"\nOdkryto: {region_name} – teren kontrolowany przez frakcję: {self.regions[region_name]}")
-        else:
-            print("\nTaki region nie istnieje.")
+        for region
