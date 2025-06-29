6ee140c29e8bc97004bc9eb50c2599df30771c5c
LowryderPL <lowryderpl921@gmail.com>
2025-06-29 20:25:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/api/inventory")
-def get_inventory():
+@app.route("/api/crafting")
+def get_recipes():
     conn = sqlite3.connect("firos_inventory.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT name, type, rarity, description FROM inventory")
+    cursor.execute("SELECT id, name, ingredients, effect FROM crafting_recipes")
     rows = cursor.fetchall()
     conn.close()
-
     return jsonify([
-        {"name": row[0], "type": row[1], "rarity": row[2], "description": row[3]}
-        for row in rows
+        {
+            "id": row[0],
+            "name": row[1],
+            "ingredients": row[2].split(','),
+            "effect": row[3]
+        } for row in rows
     ])
 
 if __name__ == "__main__":
